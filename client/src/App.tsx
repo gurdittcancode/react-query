e1717e2179c95a9f28082b0e15fe1e4a2834d83c
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2024-05-12 18:15:59 +0530

    Basics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,49 @@
-function App() {
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-  return <h1></h1>;
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+];
+
+function App() {
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+    },
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
+  return (
+    <div>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+
+      <button
+        disabled={newPostMutation.isPending}
+        onClick={() => newPostMutation.mutate('New Post')}>
+        Add Post
+      </button>
+    </div>
+  );
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
